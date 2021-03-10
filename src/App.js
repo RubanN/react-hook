f3576be81ea3a02cb6ec214770f439b425066286
RubanN <vruban556@gmail.com>
2021-03-10 16:22:03 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import Header from './components/Header'
+import Balance from './components/Balance'
+import IncomeExpenses from './components/IncomeExpenses'
+import TransactionList from './components/TransactionList'
+import AddTransaction from './components/AddTransaction'
+import './App.css'
+import {GlobalContext} from './components/context/GlobalState'
+import {AppReducer} from './components/context/AppReducer'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+ class App extends Component {
+  render() {
+    return (
+      <GlobalContext>
+        <Header/>
+        <div className = "container">
+          <Balance/>
+          <IncomeExpenses/>
+          <TransactionList/>
+          <AddTransaction/>
+        </div>
+      </GlobalContext>
+    )
+  }
 }
 
-export default App;
+export default App
